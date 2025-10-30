7425fd6c703d12a12231674971782f0b2e4afbbf
Bryan Gabriel Cardozo Benavides <itsyayx@gmail.com>
2025-10-29 20:23:50 -0500

    Delete funciones.js

--- funciones.js
+++
@@ -1,8 +0,0 @@
-function handleOptionSelected(e) {
-    toggleClass(e.target.parentNode, 'hide');
-    const id = e.target.id;
-    const newValue = e.target.textContent + ' ';
-    const titleElem = document.querySelector('.dropdown.title');
-    const icon = document.querySelector('.dropdown.title.fa');
-    // Resto de la lógica aquí...
-}
